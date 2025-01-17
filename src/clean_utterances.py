7fef234b72628749492e2e00f00d87c470ea713e
Alexander Clemen <alexander.clemen@hhu.de>
2025-01-17 18:10:03 +0100

    adds clean_utterance.py

---
+++ src/clean_utterances.py
@@ -0,0 +1,22 @@
+import re
+
+def removeDotString(sentence:str, regex = re.compile(r' \w{1,13}')) -> str:
+     #  works but \\x15 does not
+    sentence = re.sub(pattern = regex, repl = '', string = sentence)
+    return sentence
+
+
+def cleanUtterance(line:str) -> str:
+    """clean a utterance
+    
+    Args:
+        line (str): initial utterance
+        
+    Returns: cleaned utterance
+    """
+    # remove  preambel ('*CHI\t') and postamble ('\n')
+    line = line[6:].strip()
+    
+    # remove .cha specific symbol
+    line = removeDotString(line)
+    return line
