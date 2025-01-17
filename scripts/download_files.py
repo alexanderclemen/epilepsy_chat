03db1676beeacd7a77daa93790fb0a35bf5d6a3d
Alexander Clemen <alexander.clemen@hhu.de>
2025-01-17 16:58:48 +0100

    mod .gitignore, move/remove constants.py and download_files.py as files cannot be downloaded via script

--- scripts/download_files.py
+++
@@ -1,1 +0,0 @@
-# this file downloads all CHAT data from https://childes.talkbank.org/access/Clinical-Eng/POLER.html
---
+++ scripts/preprocessing.py
@@ -0,0 +1,4 @@
+# Preprocessing consists of
+# 1. Reading the data
+# 2. Cleaning the data
+# ...
