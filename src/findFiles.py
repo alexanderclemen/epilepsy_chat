5422263af195c39040715390467e9811f8f42c86
Alexander Clemen <alexander.clemen@hhu.de>
2025-01-20 17:46:39 +0100

    findCsvs -> findFiles, adds findChaFiles()

--- src/findFiles.py
+++ src/findFiles.py
@@ -3,6 +3,22 @@
 from constants import TO_POLER
 import glob
 
+def findChaFiles(desease_type:str) -> list:
+    """finds all children ids in a folder
+
+    Args:
+        folder (str): name of folder in /data/POLER/ that contains the CHAT file
+
+    Returns:
+        list: list of children ids
+    """
+    list_of_cha_files = []
+    for file in glob.glob(f"{TO_POLER}{desease_type}/*.cha"):
+        file = file[-7:-4] # extract child id from file path
+        list_of_cha_files.append(file) # append child id to list
+    return list_of_cha_files
+
+# TODO: I will turn all sentences into csv
 def findCsvs(desease_type:str) -> list:
     """finds all csv children ids in a folder
 
